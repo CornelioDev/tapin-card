60d2d0da5e73ab71540a00e6c85c76a9b98c79b9
José Cornelio <cornelio@proton.me>
2024-11-12 13:26:22 -0400

    Enhance footer layout and styling with responsive design and new utility classes

--- footer.php
+++ footer.php
@@ -1,5 +1,37 @@
-<footer>
-    Copyrigth &copy; <?php echo date('Y'); ?> Tapin Card
+<footer class="pb-8 pt-16 w-full bg-background-blue">
+    <div class="container mx-auto flex justify-between items-center mb-10">
+        <figure class="w-32">
+            <img src="<?= get_theme_file_uri('images/logo.png') ?>" alt="Tapin Logo">
+        </figure>
+        <div class="flex gap-12">
+            <nav>
+                <h3>Sobre Tapin</h3>
+                <ul>
+                    <li><a href="#">¿Qué es Tapin?</a></li>
+                    <li><a href="#">Beneficios Tapin</a></li>
+                    <li><a href="#">Como Funciona</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h3>Sobre Tapin</h3>
+                <ul>
+                    <li><a href="#">¿Qué es Tapin</a></li>
+                    <li><a href="#">Beneficios Tapin</a></li>
+                    <li><a href="#">Como Funciona</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h3>Sobre Tapin</h3>
+                <ul>
+                    <li><a href="#">¿Qué es Tapin</a></li>
+                    <li><a href="#">Beneficios Tapin</a></li>
+                    <li><a href="#">Como Funciona</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <p class="text-xs text-center text-stone-500">Copyrigth &copy; <?php echo date('Y'); ?> Tapin Card</p>
 </footer>
 </body>
+
 </html>
