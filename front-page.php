92be5e278eaa5252e881c1926d80c26e83575e47
José Manuel Cornelio <cornelio@proton.me>
2024-11-13 12:11:00 -0400

    Refactor CSS and HTML for improved responsiveness and layout adjustments

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<div class="container mx-auto lg:w-10/12">
+<div class="container mx-auto md:w-10/12">
     <!-- Hero Section -->
-     <section class="flex flex-col gap-10 lg:grid lg:grid-cols-2">
-        <div class="hero-text space-y-10 mt-5 lg:text-left text-center">
+     <section class="flex flex-col gap-10 md:grid md:grid-cols-2">
+        <div class="hero-text space-y-10 mt-5 md:text-left text-center">
             <h1>intercambia información con solo un toque</h1>
             <p>Nuestra tecnología NFC ofrece soluciones innovadoras en el intercambio de información.Tapin permite a los usuarios conectar y compartir datos con un simple toque de manera rápida y eficiente.</p>
             <a href="#" class="btn btn-lg btn-primary">Conéctate</a>
